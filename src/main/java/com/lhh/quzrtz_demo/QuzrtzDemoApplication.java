7e5025d6ce22f462a5ddb2f2af2810a96ff0367c
lihonghao <1345455432@qq.com>
2020-08-01 16:40:41 +0800

    init

---
+++ src/main/java/com/lhh/quzrtz_demo/QuzrtzDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lhh.quzrtz_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuzrtzDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuzrtzDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lhh/quzrtz_demo/QuzrtzDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhh.quzrtz_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuzrtzDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
